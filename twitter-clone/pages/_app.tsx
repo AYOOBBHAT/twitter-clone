dc2d21845f2a37fe5575c57c58baffe9434811c0
Ayoob Bhat <ayoob324005@gmail.com>
2023-09-13 15:30:47 +0530

    completed env setup& Layout

---
+++ twitter-clone/pages/_app.tsx
@@ -0,0 +1,18 @@
+import '@/styles/globals.css'
+
+import Layout from '@/components/Layout'
+import type { AppProps } from 'next/app'
+
+import { Fragment } from 'react'
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+  <Fragment>
+
+  
+  <Layout>
+    <Component {...pageProps} />
+    </Layout>
+    </Fragment>
+    
+  )}
+
