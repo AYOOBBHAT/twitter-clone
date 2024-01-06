59b65eb04f9c288757a2352a271fd640cb71798b
Ayoob Bhat <ayoob324005@gmail.com>
2024-01-06 10:22:28 +0530

    rotten oranges problem

---
+++ Desktop/DSA/grahs/rotte_oranges.cpp
@@ -0,0 +1,70 @@
+class solution
+{
+
+    int orangesRotting(vector<vector<int>> &grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        queue < pair<pair<int, int>, int> q;
+        vector<vector<int>> vis;
+
+        for (int i = 0; i < n; i++)
+        {
+
+            for (int j = 0; j < m; j++)
+            {
+
+                if (grid[i][j] == 2)
+                {
+                    q.push({{i, j}, 0});
+                    vis[i][j] = 2;
+                }
+            }
+
+            else{
+                vis[i][j]=0;
+            }
+        }
+
+        int time = 0;
+        int delrow[] = {-1, 0, +1, 0};
+        int dcol[] = {0, 1, 0, -1};
+
+        while (!q.empty())
+        {
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int t = q.front.second;
+            t=max(tm,t);
+
+
+            q.pop();
+
+            for (int i = 0; i < 4; i++)
+            {
+                int nrow = r + delrow[i];
+                int ncol = c + dcol[];
+
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1)
+                {
+                    q.puah({{nrow, ncol}, t + 1});
+                    vis[nrow][nrow] = 1;
+                }
+            }
+        }
+        // 
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+
+                if(vis[i][j]!=2 && grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+
+
+        return time;
+    }
+}
